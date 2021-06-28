356c1281af8378818d1814097d73c481b64ed777
Buland Sahu <poacher@Bulands-MacBook-Pro.local>
2021-06-29 01:36:17 +0530

    refactoring test

--- src/main/java/com/play/playwrightspring/page/ShoppingTemplate.java
+++ src/main/java/com/play/playwrightspring/page/ShoppingTemplate.java
@@ -1,8 +1,31 @@
 package com.play.playwrightspring.page;
 
+import com.play.playwrightspring.poacher.utils.SoundUtil;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public abstract class ShoppingTemplate extends AbstractBasePage {
+
+    @Value("${durationToCheck}")
+    private int duration;
 
     public abstract void navigate();
 
     public abstract boolean notifyMe();
+
+    public void checkAvailability() throws Exception {
+        this.navigate();
+        this.isDisplayed();
+        for (int i = 0; i < duration; i++) {
+            if (!this.notifyMe()) {
+                SoundUtil.tone(1000, 100, 1.0);
+                System.err.println("In Stock : " + this.getClass().getSimpleName() + " : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) + " :: " + !this.notifyMe());
+            }
+            System.out.println("Out of Stock : " + this.getClass().getSimpleName() + " : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) + " :: " + this.notifyMe());
+            page.reload();
+            this.isDisplayed();
+        }
+    }
 }
--- src/test/java/com/play/playwrightspring/tests/NotifyStock.java
+++ src/test/java/com/play/playwrightspring/tests/NotifyStock.java
@@ -1,18 +1,12 @@
 package com.play.playwrightspring.tests;
 
-import com.microsoft.playwright.Page;
 import com.play.playwrightspring.SpringBaseTestNG;
 import com.play.playwrightspring.page.AmazonPage;
 import com.play.playwrightspring.page.FLipkartPage;
 import com.play.playwrightspring.page.ShoppingTemplate;
 import com.play.playwrightspring.poacher.annotation.LazyAutowired;
-import com.play.playwrightspring.poacher.utils.SoundUtil;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import javax.sound.sampled.LineUnavailableException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class NotifyStock extends SpringBaseTestNG {
 
@@ -21,22 +15,9 @@ public class NotifyStock extends SpringBaseTestNG {
     @LazyAutowired
     private AmazonPage amazonPage;
 
-    @LazyAutowired
-    private Page page;
-
     @Test(dataProvider = "getPage")
-    public void checkStock(ShoppingTemplate shoppingTemplate) throws LineUnavailableException {
-        shoppingTemplate.navigate();
-        shoppingTemplate.isDisplayed();
-        for (int i = 0; i < 3600; i++) {
-            if (!shoppingTemplate.notifyMe()) {
-                SoundUtil.tone(1000, 100, 1.0);
-                System.err.println("In Stock : " + shoppingTemplate.getClass().getSimpleName() + " : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) + " :: " + !shoppingTemplate.notifyMe());
-            }
-            System.out.println("Out of Stock : " + shoppingTemplate.getClass().getSimpleName() + " : " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) + " :: " + shoppingTemplate.notifyMe());
-            page.reload();
-            shoppingTemplate.isDisplayed();
-        }
+    public void checkStock(ShoppingTemplate shoppingTemplate) throws Exception {
+        shoppingTemplate.checkAvailability();
     }
 
     @DataProvider(parallel = true)
